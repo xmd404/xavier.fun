ec66673995fe45304ae180f25ea55407144ff295
Xavier Duncan <xduncan@pm.me>
2019-01-27 23:31:04 -0500

    add welcome section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import '../components/styles.css';
+import '../styles/main.css';
 
 import Navbar from '../components/Navbar';
 import BannerHeader from '../components/banner-header';
+import Welcome from '../components/welcome';
 
 const IndexPage = () =>
   <div>
     <BannerHeader />
     <Navbar />
+    <Welcome />
   </div>
 
 export default IndexPage
