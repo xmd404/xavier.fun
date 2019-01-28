3b8b24c2a9cb2cfef5ed91fd09e764db6e13802b
Xavier Duncan <xduncan@pm.me>
2019-01-27 22:27:03 -0500

    add header banner

---
+++ src/components/banner-header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const BannerHeader = () =>
+    <div className="container-fluid theList">
+        <div className="container">
+        <div className="row">
+            <div className="ten columns offset-by-one column">
+            <h6 className="center">
+                <span role='img' aria-label='emoji'>👋</span> &nbsp;Try <b>theList</b>, a curated list of the best web design + development blogs.
+            </h6>
+            </div>
+        </div>
+        </div>
+    </div>
+
+export default BannerHeader
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import '../components/styles.css';
 
 import Navbar from '../components/Navbar';
-import '../components/styles.css';
+import BannerHeader from '../components/banner-header';
 
 const IndexPage = () =>
   <div>
+    <BannerHeader />
     <Navbar />
   </div>
 
