e380b3b05b2884669f1b9c94b472fa8099f09b24
Xavier Duncan <xduncan@pm.me>
2019-01-27 21:38:09 -0500

    init project, add navbar

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Navbar from '../components/Navbar';
+import '../components/styles.css';
+
+const IndexPage = () =>
+  <div>
+    <Navbar />
+  </div>
+
+export default IndexPage
