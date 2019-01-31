b7ebdca4dd89f88d3a6e966700ea025f7b314e14
Xavier Duncan <xduncan@pm.me>
2019-01-31 05:03:36 -0500

    add bio section, remove about page

--- src/components/bio.js
+++ src/components/bio.js
@@ -9,14 +9,17 @@ const FullBio = () =>
       <div className='row'>
         <br/>
         <div className='ten columns offset-by-one column'>
-          <h1 style={{ textAlign: 'center' }}><span id='options'>Hello.</span></h1>
+          <h1 style={{ textAlign: 'center' }}><span id='options'>Hello</span></h1>
           <br/>
           <h6 style={{ textAlign: 'left' }}>
             I'm Xavier, a Software Engineer & UX Developer currently based in Atlanta. I’m obsessed with all things JavaScript, <Link to='design'>design</Link>, <Link to='blog'>writing</Link>, and <a href='http://coopvision.us' target='_blank' rel='noopener noreferrer'>building great product</a>. Have ideas and want to build something cool? <Link to='contact'>Get in touch</Link>.
           </h6>
           <br/>
           <h6>
-            <span role="img" aria-label="zapEmoji">⚡️</span>&nbsp;Do Everything, See Everything. Spread Love & Share Knowledge. Move Fast. Fail Faster. Always Build Great Product.
+            <span role="img" aria-label="zapEmoji">⚡️</span>&nbsp;
+              <i>
+                Do Everything, See Everything. Spread Love & Share Knowledge. Move Fast. Fail Faster. Always Build Great Product.
+              </i>
           </h6>
           <br/>
           <Signature />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 
-import BannerHeader from '../components/banner-header';
-import Navbar from '../components/navbar';
-import Welcome from '../components/welcome';
-import ShortBio from '../components/short-bio';
-import FeaturedProjects from '../components/featured-projects';
-import FeaturedPosts from '../components/featured-posts';
-import BannerFooter from '../components/banner-footer';
+import Bio from '../components/bio';
 import Footer from '../components/footer';
 
 const IndexPage = () =>
   <div>
-    <BannerHeader />
-    <Navbar />
-    <Welcome />
-    <ShortBio />
-    <FeaturedProjects />
-    <FeaturedPosts />
-    <BannerFooter />
+    <Bio />
     <Footer />
   </div>
 
