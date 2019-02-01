88a4d882d0b62c08fae0e43b9e133e6816064070
Xavier Duncan <xduncan@pm.me>
2019-02-01 18:54:08 -0500

    reorder social icons

--- src/components/social-media.js
+++ src/components/social-media.js
@@ -7,20 +7,20 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 library.add(fab);
 
 const SocialMedia = () =>
-  <div className="twelve columns slide">
-    <a href="https://twitter.com/xmd404" target="_blank">
+  <div className='twelve columns slide'>
+    <a href='https://twitter.com/xmd404' target='_blank'>
       <FontAwesomeIcon className='fa-footer' icon={['fab', 'twitter']} />
     </a>
-    <a href="https://instagram.com/xmd404" target="_blank">
-      <FontAwesomeIcon className='fa-footer' icon={['fab', 'instagram']} />
-    </a>
-    <a href="https://www.linkedin.com/in/xduncan" target="_blank">
-      <FontAwesomeIcon className='fa-footer' icon={['fab', 'linkedin']} />
-    </a>
-    <a href="https://medium.com/@xmd404" target="_blank">
+    <a href='https://medium.com/@xmd404' target='_blank'>
       <FontAwesomeIcon className='fa-footer' icon={['fab', 'medium']} />
     </a>
-    <a href="https://github.com/xmd404" target="_blank">
+    <a href='https://instagram.com/xmd404' target='_blank'>
+      <FontAwesomeIcon className='fa-footer' icon={['fab', 'instagram']} />
+    </a>
+    <a href='https://linkedin.com/in/xduncan' target='_blank'>
+      <FontAwesomeIcon className='fa-footer' icon={['fab', 'linkedin']} />
+    </a>
+    <a href='https://github.com/xmd404' target='_blank'>
       <FontAwesomeIcon className='fa-footer' icon={['fab', 'github']} />
     </a>
   </div>
