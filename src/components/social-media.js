91f0c742630912b6daad1c8347042821977aa238
Xavier Duncan <xduncan@pm.me>
2019-02-13 11:22:55 -0500

    add font awesome icons

--- src/components/social-media.js
+++ src/components/social-media.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brand-svg-icons';
+
+library.add(fab);
+
 const linkStyle = {
   padding: '0 0.5em',
   color: '#000',
@@ -8,13 +14,13 @@ const linkStyle = {
 const SocialMedia = () =>
   <div className='twelve columns'>
     <a href='https://twitter.com/xmd404' target='_blank' style={linkStyle}>
-      twitter
+      <FontAwesomeIcon icon={['fab', 'twitter']} />
     </a>
     <a href='https://github.com/xmd404' target='_blank' style={linkStyle}>
-      github
+      <FontAwesomeIcon icon={['fab', 'github']} />
     </a>
     <a href='https://linkedin.com/in/xduncan' target='_blank' style={linkStyle}>
-      linkedin
+      <FontAwesomeIcon icon={['fab', 'linkedin']} />
     </a>
   </div>
 
