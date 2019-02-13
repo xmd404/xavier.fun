7e982285a4fe76435f77e3f0078a2740749f6304
Xavier Duncan <xduncan@pm.me>
2019-02-13 11:38:25 -0500

    bug fix

--- src/components/social-media.js
+++ src/components/social-media.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brand-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 library.add(fab);
 
