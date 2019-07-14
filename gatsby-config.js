1f54d63f4d8c20828594d05b0d3e86349b668548
Xavier Duncan <xduncan@pm.me>
2019-07-13 20:49:11 -0500

    update SEO components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Xavier Marcus Duncan | Software Engineer`,
-    description: `✨ open-sourcerer.`,
-    author: `@xmd404`,
+    title: "Xavier Marcus Duncan | Software Engineer",
+    description: "✨ open-sourcerer.",
+    author: "@xmd404",
+    url: "https://www.doe.com", // No trailing slash allowed!
+    image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
+    twitterUsername: "@oxmd404",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
