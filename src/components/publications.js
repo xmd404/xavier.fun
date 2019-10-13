bf39e2121bcf21cfe63adb790b7143aaca3da8f8
Xavier Duncan <xduncan@pm.me>
2019-10-12 21:50:50 -0500

    add publications to contact page

---
+++ src/components/publications.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Publications = () => (
+	<div className="container-fluid" style={{ padding: '0 0 15px' }}>
+		<div className="container">
+			<div className="row">
+				<div className="eight columns offset-by-two columns">
+          <h6>Thanks for reaching out! Read one of my recent publicatinos in the meantime.</h6>
+					<ul style={{ listStyle: 'none', padding: 0 }}>
+						<li>
+							2016 &nbsp; 
+              <a 
+                href='https://medium.com/@XMD404/why-a-career-in-web-development-f2eee2d5f4d8'
+                target='_blank'>
+                  Why a career in web development?
+              </a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default Publications;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from '../components/header';
 import ContactForm from '../components/contact-form';
+import Publications from '../components/publications';
 import Footer from '../components/footer';
 
 const ContactPage = () =>
   <div>
     <Header />
     <ContactForm />
+    <Publications />
     <Footer />
   </div>
 
