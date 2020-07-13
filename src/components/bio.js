9e3a01c735e0fdc38189b4dba915fb1408beb668
Xavier Duncan <xduncan@pm.me>
2020-07-13 07:55:21 -0500

    update bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -17,7 +17,11 @@ const Bio = () =>
       <div className='row'>
         <div className='eight columns offset-by-two columns'>
           <h6>
-            Software Engineer + Product Designer currently based in ATX. I’m obsessed with all things JavaScript, design, <a href='https://medium.com/@xmd404' target='_blank'>writing</a>, and <a href='https://simplestock.cc' target='_blank'>building great products</a>. Interested in having me join your project?
+            Engineer, founder, & angel investor based in ATX. I’m obsessed with all things venture, photography, <a href='https://medium.com/@xmd404' target='_blank'>writing</a>, and <a href='https://peace.vc' target='_blank'>collaborating on projects that shape the world.</a>.
+            <br/><br/>
+            Currently building <a href="https://mobiletechrx.com">MTRX</a> & <a href="https://peace.vc">Peace Ventures</a>.
+            <br/><br/>
+            Interested in having me join your project?
             <br/><br/>
             <a href='https://calendly.com/xavierduncan' target='_blank' rel="noopener noreferrer">
               <button className='btn-blue'>
