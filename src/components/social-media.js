d35371014ee8671d36999e3fe3fdb4e6ad28d8bf
Xavier Duncan <xduncan@pm.me>
2020-07-14 14:20:15 -0500

    fix flashing font-awesome icons

--- src/components/social-media.js
+++ src/components/social-media.js
@@ -3,7 +3,10 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css"
 
+config.autoAddCss = false
 library.add(fab);
 
 const linkStyle = {
