4a98e857ba7d705c8bc83b47b98ac6da5dfe1dcf
xmd404 <xmd404@pm.me>
2020-10-13 22:13:22 -0500

    update bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,29 +1,44 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import Signature from '../components/signature';
+import React from 'react'
+import { Link } from 'gatsby'
+import Signature from '../components/signature'
 
 const divStyle = {
   padding: '0 0 35px',
-};
+}
 
 const polyStyle = {
   textAlign: 'center',
-};
+}
 
-const Bio = () =>
-  <div className='container-fluid' style={divStyle}>
-    <div className='container'>
-      <hr/>
-      <div className='row'>
-        <div className='eight columns offset-by-two columns'>
+const Bio = () => (
+  <div className="container-fluid" style={divStyle}>
+    <div className="container">
+      <hr />
+      <div className="row">
+        <div className="eight columns offset-by-two columns">
           <h6>
             Engineer, founder, & angel investor based in ATX.
-            <br/><br/>
-            I’m obsessed with all things venture, photography, <a href='https://medium.com/@xmd404' target='_blank'>writing</a>, and collaborating on projects that shape the world.
-            <br/><br/>
-            Currently building: <a href="https://mobiletechrx.com" target='_blank'>MTRX</a>, <a href="https://peace.vc" target='_blank'>Peace Ventures</a>, & <a href="https://votewithdollars.to/change-the-world" target='_blank'>#VoteWithDollars</a>.
-            <br/><br/>
-            Shoot me an <a href="mailto:xmd404@pm.me">email</a> or <a href="https://twitter.com/xmd404" target="_blank">DM</a>, let's break things 💥
+            <br />
+            <br />
+            I’m obsessed with all things venture, photography,{' '}
+            <a href="https://medium.com/@xmd404" target="_blank">
+              writing
+            </a>
+            , and collaborating on projects that shape the world.
+            <br />
+            <br />
+            Currently building: <a href="#">Ethni.ca</a> &{' '}
+            <a href="https://peace.vc" target="_blank">
+              Peace Ventures
+            </a>
+            .
+            <br />
+            <br />
+            Shoot me an <a href="mailto:xmd404@pm.me">email</a> or{' '}
+            <a href="https://twitter.com/xmd404" target="_blank">
+              DM
+            </a>
+            , let's break things 💥
             {/* <a href='https://calendly.com/xavierduncan' target='_blank' rel="noopener noreferrer">
               <button className='btn-blue'>
                 📲 &nbsp;Schedule a call
@@ -35,11 +50,12 @@ const Bio = () =>
               </button>
             </Link> */}
           </h6>
-          <br/>
+          <br />
           <Signature />
         </div>
       </div>
     </div>
   </div>
-  
-export default Bio;
+)
+
+export default Bio
